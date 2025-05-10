de3aead4a449af5ad0561c960a79a75a7d7c01ca
Jamie Andrews <andrews_j@live.co.uk>
2025-05-10 16:27:52 +0100

    next update

--- src/components/MdxLayout/index.tsx
+++ src/components/MdxLayout/index.tsx
@@ -4,7 +4,7 @@ export default function MdxLayout({ children }: { children: React.ReactNode }) {
   // Create any shared layout or styles here
   return (
     <>
-      <div className="mdxlayout">{children}</div>;
+      <div className="mdxlayout">{children}</div>
     </>
   );
 }
