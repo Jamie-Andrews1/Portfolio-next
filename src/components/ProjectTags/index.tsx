2fc3ae29c8fdaf05195c38166d983fe35b4338ab
jamie andrews <andrews_j@live.co.uk>
2026-05-15 19:00:57 +0100

    updated styles for home and posts page

--- src/components/ProjectTags/index.tsx
+++ src/components/ProjectTags/index.tsx
@@ -4,7 +4,13 @@ import { Link } from "next-view-transitions";
 export default async function page() {
   const projects = await getProjects();
   return (
-    <ul style={{ listStyle: "none", marginLeft: "10px" }}>
+    <ul
+      style={{
+        listStyle: "none",
+        marginLeft: "10px",
+        backdropFilter: "blur(3px)",
+      }}
+    >
       {projects.map((p) => (
         <Link href={`/${p.slug}`} key={p.title}>
           <li>{p.stack}</li>
